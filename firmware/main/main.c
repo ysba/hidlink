58476c13561db87941aecede444be06eb1d183af
Yuri Arbeletche <ysba000@gmail.com>
2023-08-21 21:08:48 -0300

    added source files

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,19 +1,8 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
+#include "main.h"
 
-#include <stdio.h>
-#include <inttypes.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
 
-void app_main(void)
-{
+void app_main(void) {
+    
     printf("Hello world!\n");
 
     /* Print chip information */
---
+++ firmware/main/main.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+
+#include "hidlink.h"
+#include "bluetooth.h"
+#include "usb.h"
