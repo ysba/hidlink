01efa43b263c1399a7c2ab98b5b5a28a5e628a9b
Yuri Arbeletche <ysba000@gmail.com>
2023-10-09 22:59:01 -0300

    added rp2040 fw folder

---
+++ firmware_esp32/main/main.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <inttypes.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "sdkconfig.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+
+#include "hidlink.h"
+#include "bt_hid_host.h"
+#include "usb.h"
