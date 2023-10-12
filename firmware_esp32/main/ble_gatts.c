f217ae5da5ef4151a7422f6b51b4b10a7a946ffb
Yuri Arbeletche <ysba000@gmail.com>
2023-10-12 20:41:27 -0300

    [esp32] added list of expected hid peripherals + scan results

--- firmware_esp32/main/ble_gatts.c
+++ firmware_esp32/main/ble_gatts.c
@@ -23,7 +23,7 @@ struct gatts_profile_inst{
 static struct gatts_profile_inst gatt_profile_table[GATT_PROFILE_INDEX_MAX] = {
     [GATT_PROFILE_INDEX_SPP] = {
         .gatts_cb = gatts_cdtp_cb,
-        .gatts_if = ESP_GATT_IF_NONE,       /* Not get the gatt_if, so initial is ESP_GATT_IF_NONE */
+        .gatts_if = ESP_GATT_IF_NONE,
     },
 };
 
@@ -46,17 +46,9 @@ void ble_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
             break;
         }
         
-        // case ESP_GATTS_READ_EVT:
-        //     ESP_LOGD(TAG, "ESP_GATTS_READ_EVT"); 
-        //     break;
-
         case ESP_GATTS_WRITE_EVT: 
             ESP_LOGD(TAG, "ESP_GATTS_WRITE_EVT"); 
             break;
-
-        // case ESP_GATTS_EXEC_WRITE_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_EXEC_WRITE_EVT"); 
-        //     break;
 
         case ESP_GATTS_MTU_EVT: 
             ESP_LOGD(TAG, "ESP_GATTS_MTU_EVT"); 
@@ -66,37 +58,9 @@ void ble_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
             ESP_LOGD(TAG, "ESP_GATTS_CONF_EVT"); 
             break;
 
-        // case ESP_GATTS_UNREG_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_UNREG_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_CREATE_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_CREATE_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_ADD_INCL_SRVC_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_ADD_INCL_SRVC_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_ADD_CHAR_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_ADD_CHAR_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_ADD_CHAR_DESCR_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_ADD_CHAR_DESCR_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_DELETE_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_DELETE_EVT"); 
-        //     break;
-
         case ESP_GATTS_START_EVT: 
             ESP_LOGD(TAG, "ESP_GATTS_START_EVT"); 
             break;
-
-        // case ESP_GATTS_STOP_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_STOP_EVT"); 
-        //     break;
 
         case ESP_GATTS_CONNECT_EVT: 
             ESP_LOGD(TAG, "ESP_GATTS_CONNECT_EVT"); 
@@ -104,43 +68,12 @@ void ble_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
 
         case ESP_GATTS_DISCONNECT_EVT: 
             ESP_LOGD(TAG, "ESP_GATTS_DISCONNECT_EVT"); 
+            esp_bt_gap_cancel_discovery();
             break;
-
-        // case ESP_GATTS_OPEN_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_OPEN_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_CANCEL_OPEN_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_CANCEL_OPEN_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_CLOSE_EVT:
-        //     ESP_LOGD(TAG, "ESP_GATTS_CLOSE_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_LISTEN_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_LISTEN_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_CONGEST_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_CONGEST_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_RESPONSE_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_RESPONSE_EVT"); 
-        //     break;
 
         case ESP_GATTS_CREAT_ATTR_TAB_EVT: 
             ESP_LOGD(TAG, "ESP_GATTS_CREAT_ATTR_TAB_EVT"); 
             break;
-
-        // case ESP_GATTS_SET_ATTR_VAL_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_SET_ATTR_VAL_EVT"); 
-        //     break;
-
-        // case ESP_GATTS_SEND_SERVICE_CHANGE_EVT: 
-        //     ESP_LOGD(TAG, "ESP_GATTS_SEND_SERVICE_CHANGE_EVT"); 
-        //     break;        
 
     	default: 
             ESP_LOGW(TAG, "UNEXPECTED GATTS EVENT: %d", event); 
@@ -151,7 +84,6 @@ void ble_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
     // iterate through all profiles declared in gatt_profile_table
     int idx;
     for (idx = 0; idx < GATT_PROFILE_INDEX_MAX; idx++) {    
-        /* ESP_GATT_IF_NONE, not specify a certain gatt_if, need to call every profile cb function */
         if (gatts_if == ESP_GATT_IF_NONE || gatts_if == gatt_profile_table[idx].gatts_if) {
             if (gatt_profile_table[idx].gatts_cb) {
                 gatt_profile_table[idx].gatts_cb(event, gatts_if, param);
