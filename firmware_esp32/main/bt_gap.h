b545a91d56174d2d4c92646e9825c7e20302a631
Yuri Arbeletche <ysba000@gmail.com>
2024-01-02 21:59:24 -0300

    removed esp32 and rp2040 projects from repo

--- firmware_esp32/main/bt_gap.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BT_GAP__
-#define __BT_GAP__
-
-void bt_gap_event_handler(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param);
-
-char *bda2str(esp_bd_addr_t bda, char *str, size_t size);
-
-#endif
